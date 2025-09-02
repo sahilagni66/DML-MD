074c1a8ecf34ff3385ffc36ca3c249bf2dad2c02
DML <daudymussa1705@gmail.com>
2025-09-02 20:40:19 +0300

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  SESSION_ID: "",  // add your Session Id here
+  
+  OWNER_NUMBER: "255622220680", // put your phone number here
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Africa/Dar-es-salaam" //put your country timeZone....leave blank if u don't know.
+};
