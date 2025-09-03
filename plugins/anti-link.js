6916bdafa94fdbde2b6b323f452a607f175df986
DML <daudymussa1705@gmail.com>
2025-09-03 22:57:53 +0300

    Update anti-link.js

--- plugins/anti-link.js
+++ plugins/anti-link.js
@@ -39,6 +39,7 @@ cmd({
       /https?:\/\/(?:www\.)?vimeo\.com\/\S+/gi,           // Vimeo links
       /https?:\/\/(?:www\.)?dailymotion\.com\/\S+/gi,     // Dailymotion links
       /https?:\/\/(?:www\.)?medium\.com\/\S+/gi           // Medium links
+      /https?:\/\/(?:api\.github.com\/\/S+/gi,             // github links
     ];
 
     // Check if message contains any forbidden links
